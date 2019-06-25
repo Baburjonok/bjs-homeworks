a8edae5b4b1d11906e127043ee9a25f103726b71
Elena Bikh <baburjonok@mail.ru>
2019-06-25 22:20:43 +0500

    first commit variables

---
+++ variables/main.js
@@ -0,0 +1,29 @@
+let algebra; //Задание 1
+let geography;
+let physics;
+
+algebra = 5;
+geography = 4;
+physics = 4;
+
+let averageRating = (algebra + geography + physics) / 3;
+
+console.log (averageRating);
+
+let name = 'Елена'; //Задание 2
+
+let messag = `Привет, мир! Меня зовут ${name}.`
+
+console.log (messag);
+
+let x; //Задание 3
+let y;
+let z;
+
+x = 2;
+y = 22;
+z = 0;
+
+let result = x * y + 5 * z + x - 1;
+
+console.log(result);
