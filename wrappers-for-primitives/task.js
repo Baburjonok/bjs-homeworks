5b2f66a14fb105bfc696c3ac8a9e74421b4bdac5
Elena Bikh <baburjonok@mail.ru>
2019-07-08 19:00:26 +0500

    first commit wrappers-for-primitives

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -10,10 +10,38 @@ function calculateMortgage() {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+    let today = new Date();
+    date = Math.floor((Date.parse(date) - Date.now()) / 2592000000);
 
+    let P = percent / 1200;
+    let data = {
+        percent: percent,
+        contribution: contribution,
+        amount: amount,
+        date: date
+    };
+  
+    let dataAfterParse = {};
+
+    for (let key in data) {
+        dataAfterParse[key] = parseFloat(data[key]);
+        if (isNaN(parseFloat(data[key])) === true) {
+            totalAmount = `Параметр ${key} содержит неправильное значение ${data[key]} .`;
+            return totalAmount; 
+        };
+    };
+
+    P = dataAfterParse.percent / 1200;
+
+    monthlyPay = (dataAfterParse.amount - dataAfterParse.contribution) * (P + P / (Math.pow((1 + P), dataAfterParse.date) - 1));
+    totalAmount = monthlyPay * dataAfterParse.date;
+
+    return totalAmount;
     // код для задачи №1 писать здесь
     //return totalAmount;
 }
+
+
 
 function sayHello() {
     let name = window.personName.value;
@@ -23,6 +51,14 @@ function sayHello() {
 }
 
 function getGreeting(name) {
+    if (name) {
+        greeting = `Привет, Мир! Меня зовут ${name}`;
+      } else {
+        greeting = "Привет, Мир! Меня зовут Аноним";
+      };
+          
+      return greeting;
+    
     // код для задачи №2 писать здесь
     //return greeting;
 }
