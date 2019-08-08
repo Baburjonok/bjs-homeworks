a12bf81683e97013dd3b1d767099cd2603f33df8
Elena Bikh <baburjonok@mail.ru>
2019-08-08 11:19:35 +0500

    third commit array-proto-hocs

--- array-proto-hocs/main.js
+++ array-proto-hocs/main.js
@@ -4,50 +4,44 @@ function compareArrays(arr1, arr2) {
   return arr1.length === arr2.length && arr1.every((value, i) => value === arr2[i]);
 }
 
-function g(memoizeSum) {  //Функция взята для примера
-  return memoizeSum.result === 7;
-}
+function memoize(fn, limit) {
+    let results = []; 
+      
+    return function(){
+      let arr1 = Array.from(arguments); 
 
-function memoize(fn, limit){
-  let results = [];
-  return function(numbers){
-    let arr1 = numbers;
-    //console.log(arr1);
-    let sumArg = 0;
-    numbers.forEach(function(number) {
-    sumArg += number;
-    });
+      let index = results.findIndex(element => compareArrays(arr1, element.args));  
 
-    let resultSum = {
-      args: numbers, 
-      result:sumArg
-    }
-    
-    //console.log(resultSum);
-    results.push(resultSum);
-    //console.log(results);
-    
-    const task1 = results.find(fn);        //Это пробный код, с пробной функцией g он работает,
-    //console.log(task1);                  //а вот с функцией compareArrays не работает.
-    const task2 = results.findIndex(fn);   //В идеале хотелось бы найти именно индекс массива с аргументами в памяти,
-    //console.log(task2);                  //но при использовании compareArrays все равно возникает 
-	                                       //ошибка TypeError: arr1.every is not a function
-	
-    if (task2 >= 0) {
-      console.log(`Сумма элементов равна ${results[task2].result}`);
-    //};
-    } else {    
-    console.log(`Сумма элементов равна ${sumArg}`);
-    };
-
-    if (results.length > limit) {
-      results.shift();
+      let resultFn;
+        if (index === -1) {
+            resultFn = fn(...arr1);
+            let res = {args:arr1, result:resultFn};
+            if(results.length >= limit) {
+                results.shift();
+            }
+            results.push(res);
+        }
+        else {
+            resultFn = results[index].result;
+        }
+        console.log("Сумма аргументов равна " + resultFn);
+                
     }
   }
+
+ function sum() {
+    let arr3 = Array.from(arguments);
+    let sumArg = 0;
+    arr3.forEach(function(number) {sumArg += number; });
+    return sumArg;
+    
 }
 
-let sum = memoize(g, 3);
-//sum([1, 2, 3]);
-//sum([1, 2, 4]);
-//sum([1, 2, 4, 8]);
-//sum([1, 2, 3]);
+
+const mSum = memoize(sum, 4);
+mSum(1, 3, 2);
+mSum(3, 4, 2, 5, 7);
+mSum(7, 1);
+mSum(3, 4);
+mSum(7, 1);
+
